ece93cb5426e1a7af806541252677694e00a4592
Elliott Balsley <3991046+llamafilm@users.noreply.github.com>
2025-11-16 23:19:15 -0800

    cookiecutter project setup

---
+++ high_desert_happenings/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from high_desert_happenings.users.models import User
+from high_desert_happenings.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
